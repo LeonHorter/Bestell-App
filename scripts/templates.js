fa30fcbb5cddaeaa3bd381d707e92a7969c8313b
Leon Horter <leon.horter@icloud.com>
2026-07-27 22:46:09 +0200

    added database content

---
+++ scripts/templates.js
@@ -0,0 +1,17 @@
+function getMealCardTemplate() {
+    return `<article class="mealCard">
+                <img src="./assets/img/menu-01.png" alt="Veggie mushroom black burger">
+                <div class="mealCardContent">
+                    <div class="mealCardDescription">
+                        <div>
+                            <h3>Veggie mushroom black burger</h3>
+                            <p>Mixed green salad, Tomatoes, Edamame, Mushrooms</p>
+                        </div>
+                        <h3>16,90 €</h3>
+                    </div>
+                    <div class="actions">
+                        <button class="addBasketBtn">Add to basket</button>
+                    </div>
+                </div>
+            </article>`;
+}
