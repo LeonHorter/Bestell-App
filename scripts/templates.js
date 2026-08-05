6ad9e811cc5f6543131e65e22a7d7b5bed1983f1
Leon Horter <leon.horter@icloud.com>
2026-08-06 00:57:28 +0200

    added content and style for basket

--- scripts/templates.js
+++ scripts/templates.js
@@ -2,11 +2,11 @@ function getMealCardTemplate(meal) {
     return `<article class="mealCard">
                 <img src="./assets/img/${meal.imgName}" alt="${meal.name}">
                 <div class="mealCardContent">
-                    <div class="mealCardTextBox">
+                    <div class="mealCardLeftBox">
                         <h3>${meal.name}</h3>
                         <p class="mealCardDescription">${meal.description}</p>
                     </div>
-                    <div class="actions">
+                    <div class="mealCardRightBox">
                         <h3>${formatToCurrency(meal.price)}</h3>
                         <button class="addBasketBtn">Add to basket</button>
                     </div>
